abf1d594429669094c7ae8502a9504adff2a57f2
Nitin Kumar <nitinvupk83@gmail.com>
2021-01-19 14:40:27 +0530

    Form builder with json and react redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
+import React from 'react';
+import { connect} from 'react-redux'
 import './App.css';
 
-function App() {
-  return (
+class  App extends React.Component {
+  constructor(props){
+  super(props);
+  this.state={
+   }
+  }
+
+  componentDidMount() {
+    const {data} = this.props
+    const formData = data.flows.main.map(item => {
+      return (
+        data.components[item]
+      )
+    })
+    this.setState({formData})
+  }
+
+  handleInputChange = (e) => {
+    this.props.updateInputField(e.target.value)
+  }
+
+  onCovidValueChange = (e) => {
+    this.props.updateCovidField(e.target.value)
+  }
+
+  onTraveledValueChange = (e) => {
+    this.props.updateTraveledField(e.target.value)
+  }
+
+  handleShowData = () => {
+    const data = JSON.stringify(this.props.data)
+    alert(data);
+  }
+
+  render() {
+    const {strings} = this.props.data
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.formData && this.state.formData.map((item,index) => {
+        switch(item.type) {
+          case "textbox-text":
+            return (
+              <div className="container">
+                <label><b>{strings.en.s8} :</b></label> 
+                <input 
+                  type="text"  
+                  className="inputfield"
+                  value={strings.en.s9?strings.en.s9:""}
+                  required = {item.isMandatory}
+                  onChange={this.handleInputChange}
+                  >
+                </input>
+              </div>
+            )
+          case "radio-group" :
+            return(
+              <div>
+                {Object.keys(item.items).length === 3 
+                  ? 
+                  <div>
+                    <div className="radio">
+                      <h3> {strings.en.s1}</h3>
+                      <label>
+                        <input
+                          type="radio"
+                          value={strings.en.s2}
+                          checked={strings.en.s10 === "Yes"}
+                          onChange={this.onCovidValueChange}
+                        />
+                        {strings.en.s2}
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input
+                          type="radio"
+                          value={strings.en.s3}
+                          checked ={strings.en.s10 === "No"}
+                          onChange={this.onCovidValueChange}
+                        />
+                        {strings.en.s3}
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input
+                          type="radio"
+                          value={strings.en.s4}
+                          checked ={strings.en.s10 === "Not sure"}
+                          onChange={this.onCovidValueChange}
+                        />
+                        {strings.en.s4}
+                      </label>
+                    </div>
+                  </div>
+                  :<div>
+                    <div className="radio">
+                      <h3>  {strings.en.s5}</h3>
+                      <label>
+                        <input
+                          type="radio"
+                          value={strings.en.s6}
+                          checked ={strings.en.s11 === "Yes"}
+                          onChange={this.onTraveledValueChange}
+                        />
+                        {strings.en.s6}
+                      </label>
+                    </div>
+                    <div className="radio">
+                      <label>
+                        <input
+                          type="radio"
+                          value={strings.en.s7}
+                          checked ={strings.en.s11 === "No"}
+                          onChange={this.onTraveledValueChange}
+                        />
+                        {strings.en.s7}
+                      </label>
+                    </div>
+                  </div>
+                }
+              </div>
+            )
+          }
+        })
+      }
+      <div className="submit">
+      <input
+      className="submit_button"
+      type = "button"
+      value="submit"
+      onClick={this.handleShowData}
+      >
+      </input>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+function mapStateToProps (state) {
+  return {
+    data: state.data
+   };
+  }
+
+
+  function mapDispatchToProps(dispatch) {
+    return {
+      updateInputField: (value) => dispatch({type: 'UPDATEINPUT',data:value}),
+      updateCovidField: (value) => dispatch({type: 'UPDATECOVIDTEST',data:value}),
+      updateTraveledField: (value) => dispatch({type: 'UPDATETRAVELEDFIELD',data:value})
+     };
+     
+    }
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,90 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+const getInitialState = () => ({
+  data: {
+      "flows":{ "main":[
+      "component3",
+      "component1",
+      "component2", ] }, "components":{ "component1":{
+      "type":"radio-group", "title":"s1", "items":{ "item3":{
+      "title":"s2", }, "item2":{
+      "title":"s3", }, "item1":{
+      "title":"s4" }
+      }
+      }, "component2":{
+      "type":"radio-group", "title":"s5", "items":{ "item1":{
+      "title":"s6" }, "item2":{
+      "title":"s7" } }
+      }, "component3":{ "type":"textbox-text", "isMandatory":true, "title":"s8" }
+      }, "strings":{
+      "en":{ "s1":"Have you been tested positive for COVID in last 14 days?", "s2":"Yes", "s3":"No", "s4":"Not sure", "s5":"Have you Traveled outside in last 14 days?", "s6":"Yes", "s7":"No", "s8":"Enter your name",
+      } }
+      
+      }
+});
+
+
+const formData = (state =  getInitialState(), action) => {
+  switch(action.type) {
+     case 'UPDATEINPUT':
+       return {
+         ...state,
+         data:{
+           ...state.data,
+           strings: {
+             ...state.data.strings,
+             en: {
+              ...state.data.strings.en,
+              s9:action.data
+             }
+           }
+          }
+         }
+   
+     case 'UPDATECOVIDTEST':
+        return {
+          ...state,
+          data:{
+            ...state.data,
+            strings: {
+              ...state.data.strings,
+              en: {
+               ...state.data.strings.en,
+               s10:action.data
+              }
+            }
+           }
+          }
+      case 'UPDATETRAVELEDFIELD':
+          return {
+            ...state,
+            data:{
+              ...state.data,
+              strings: {
+                ...state.data.strings,
+                en: {
+                 ...state.data.strings.en,
+                 s11:action.data
+                }
+              }
+             }
+            }
+
+     default:
+       return state;
+   }
+};
+const store = createStore(formData);
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
